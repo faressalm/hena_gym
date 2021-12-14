a4788ea57fde44413a4b8f43b594f74e312d961c
fares waheed <faressalm44@hotmail.com>
2021-12-14 22:34:20 +0200

    add colors and logo

---
+++ lib/constants/my_gui.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color white = Color(0xFFF7F7F7);
+  static const Color lightGray = Color(0xffB2B2B2);
+  static const Color gray = Color(0xff6F6F6F);
+  static const Color grayBlue = Color(0xff373B59);
+  static const Color darkBlue = Color(0xff0C1248);
+  static const Color red = Color(0xffd03737);
+  static const Color darkRed = Color(0xff860C0C);
+  static const Color cyan = Color(0xff74E0EF);
+}
